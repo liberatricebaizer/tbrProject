071dba8a79f4fcbf743707004cf454ea1f3665e1
liberatrice bayizere <92096502+liberatricebaizer@users.noreply.github.com>
2023-09-15 07:18:48 +0200

    U

--- clients/src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSliceReducer from "./userSlice";
-import rentSliceReducer from "./rentSlice";
-export const store = configureStore({
-  reducer: {
-    user: userSliceReducer,
-    rent: rentSliceReducer,
-  },
-});
